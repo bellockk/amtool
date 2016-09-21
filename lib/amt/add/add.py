7a790b61d8009abd2363e759a638e4ca8a5efe3f
bellockk <bellockk@segmail.com>
2016-09-21 19:39:29 -0400

    Including order preserving read/write of yaml files

--- lib/amt/add/add.py
+++ lib/amt/add/add.py
@@ -17,14 +17,22 @@ It defines classes_and_methods and a command line interface
 __all__ = ['add']
 
 import os
+import sys
 import uuid
-import yaml
 
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+LIB_PATH = os.path.join(SCRIPT_PATH, '..', '..')
+sys.path.insert(0, LIB_PATH)
+
+from amt.file_io import safe_dump
 
 def add(targets, verbose=1):
     for target in targets:
+        dirname = os.path.dirname(target)
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
         f_obj = open(target, 'w')
-        f_obj.write(yaml.dump({'uid': str(uuid.uuid4())},
+        f_obj.write(safe_dump({'uid': str(uuid.uuid4())},
                               default_flow_style=False))
         f_obj.close()
     return 0
