434281bdc9a9efe0d74150807a01f68295f41ce1
bellockk <bellockk@segmail.com>
2019-06-26 08:19:09 -0400

    Including base render capability

--- amt/cli.py
+++ amt/cli.py
@@ -9,6 +9,7 @@ import yaml
 from amt import canonical as canon
 from amt import load
 from amt import uid as amtuid
+from amt import render as amtrender
 from amt import MetaDict
 from amt import MetaList
 from pkg_resources import iter_entry_points
@@ -57,6 +58,19 @@ def uid(path):
                          lambda dumper, data: dumper.represent_sequence(
                              'tag:yaml.org,2002:seq', data))
     click.echo(yaml.dump(amtuid(path), default_flow_style=False))
+    return 0
+
+
+@main.command()
+@click.argument('PATH')
+def render(path):
+    yaml.add_representer(MetaDict,
+                         lambda dumper, data: dumper.represent_mapping(
+                             'tag:yaml.org,2002:map', data.items()))
+    yaml.add_representer(MetaList,
+                         lambda dumper, data: dumper.represent_sequence(
+                             'tag:yaml.org,2002:seq', data))
+    click.echo(yaml.dump(amtrender(path), default_flow_style=False))
     return 0
 
 
--- amt/render/render.py
+++ amt/render/render.py
@@ -14,15 +14,44 @@ It defines classes_and_methods and a command line interface
 """
 import os
 import sys
+import copy
 import logging
+from mako.template import Template
 
 __all__ = ['render']
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(os.path.dirname(SCRIPT_PATH), 'load'))
+sys.path.insert(0, os.path.join(os.path.dirname(SCRIPT_PATH), 'uid'))
 from load import load
+from uid import uid
 
 
-def render(target, engine='mako'):
+def _render(artifacts, uids):
+    """
+    Recursive helper function for diving into dictionaries of dictionaries
+    searching for unique id's.
+
+    Args:
+        artifacts (dict): Input dictionary.
+
+    Returns:
+        dict: returned dictionary
+    """
+    if isinstance(artifacts, dict):
+        for key, value in artifacts.items():
+            if isinstance(value, (dict, list, tuple)):
+                _render(value, uids)
+            elif isinstance(value, str):
+                artifacts[key] = Template(value).render(UID=uids)
+    elif isinstance(artifacts, (list, tuple)):
+        for index, value in enumerate(artifacts):
+            if isinstance(value, (dict, list, tuple)):
+                _render(value, uids)
+            elif isinstance(value, str):
+                artifacts[index] = Template(value).render(UID=uids)
+
+
+def render(source, engine='mako'):
     """
     Render a set of artifacts.
 
@@ -32,7 +61,7 @@ def render(target, engine='mako'):
     structure.
 
     Args:
-        target (str): The directory or file to be loaded.
+        source (str): The directory or file to be loaded.
 
     Kwargs:
         verbose (int): Level to perform logging at.
@@ -41,5 +70,10 @@ def render(target, engine='mako'):
         dict.  The fully read data structure containing all artifacts from the
         loaded target.
     """
-    logging.debug('Rendering Target: %s', target)
-    return load(target)
+    logging.debug('Rendering: %s', source)
+    if isinstance(source, str):
+        data = load(source)
+    else:
+        data = copy.deepcopy(source)
+    _render({'': data}, uid(source))
+    return data
