28cd86fea9bce0f5d50e68172acc66d60c77785d
bellockk <bellockk@segmail.com>
2019-06-27 13:58:59 -0400

    Cleaning up documentation

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.imgconverter']
 
 # Github repository
-issues_github_path = 'bellockk/alphashape'
+issues_github_path = 'bellockk/amtool'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
